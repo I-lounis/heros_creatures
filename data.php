bc1d22cd2ae2049bd56cf8b35a091e5185e45d76
I-lounis <ilyes.lounicipro@gmail.com>
2025-09-08 16:56:58 +0200

    update

---
+++ data.php
@@ -0,0 +1,35 @@
+<?php
+    require_once 'partials/head.php';
+?>
+
+
+<?php
+
+$heros = [
+    [
+        'id' => 1,
+        'name' => 'Silune',
+        'magic_power' => 'capable de créer des illusions',
+        'picture' => 'Silune_img.png'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Revas',
+        'magic_power' => 'échanger de place avec quelqu\'un/objet',
+        'picture' => 'Revas_2_img.png'
+    ],
+    [
+        'id' => 3,
+        'name' => 'Nori',
+        'magic_power' => 'faire des origamis (mais vraiment trop bien)',
+        'picture' => 'Nori_img.png'
+    ]
+    ];
+?>
+
+<?php
+    require_once 'partials/head.php';
+?>
+
+
+
